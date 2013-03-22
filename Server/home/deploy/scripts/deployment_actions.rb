9f071abd06ec6c7bf314e39d639d0c32bd0cc5e8
Giancarlo Mariot ZG <sentient.06+developmentZG@gmail.com>
2013-03-22 10:58:36 +0000

    Working on systemCmd method

--- Server/home/deploy/scripts/deployment_actions.rb
+++ Server/home/deploy/scripts/deployment_actions.rb
@@ -150,10 +150,15 @@ class DeploymentActions
   #-------------------------------------------------------------------------------
   # System actions
 
+  # Returns a process object.
   def systemCmd(commandStr)
 
     print "Executing '#{commandStr}'..."
     output = `#{commandStr} 2>&1`
+    # $? -> process, i.e
+    # #<Process::Status: pid 1612 exit 0>
+    # #<Process::Status: pid 1620 exit 2>
+    result = $?
 
   end
 
