701b9d69cad02d51550164b039eac6a0591c04cd
Giancarlo Mariot ZG <sentient.06+developmentZG@gmail.com>
2013-04-12 17:39:47 +0100

    Splitting into smaller files

--- Server/home/deploy/scripts/lib/system.rb
+++ Server/home/deploy/scripts/lib/system.rb
@@ -6,8 +6,8 @@ class System
 
   # Returns a process object.
   def execute(command)
-    # print "Executing '#{commandStr}'..."
-    output = `#{commandStr} 2>&1`
+    # print "Executing '#{command}'..."
+    output = `#{command} 2>&1`
     # $? -> process, i.e
     # #<Process::Status: pid 1612 exit 0>
     # #<Process::Status: pid 1620 exit 2>
