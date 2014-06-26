b21a27bb5ea8409d0014c6863966c73d82556d7c
Giancarlo Mariot <sentient.06@gmail.com>
2014-06-26 13:16:00 +0100

    Fixing bot script

--- Server/home/bot/trigger.sh
+++ Server/home/bot/trigger.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 PATH=$PATH:/usr/local/rvm/bin # Add RVM to PATH for scripting
 [[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm" # Load RVM function
-sudo -u deploy -E ruby /home/deploy/scripts/trigger.rb $@
+ruby=`which ruby`
+sudo -u deploy -E $ruby /home/deploy/scripts/trigger.rb $@
